7a47216ee2d2170809fdc665313bd27dcf23ebaa
ParakotaSuresh9 <126375390+ParakotaSuresh9@users.noreply.github.com>
2023-03-25 17:13:43 +0530

    Add files via upload

---
+++ Assignment-4.py
@@ -0,0 +1,17 @@
+#solution for question1
+num=10
+add=lambda x:x+25
+print(add(num))
+
+
+
+#solution for question2
+sample_list=[1, 2, 3, 4, 5, 6, 7]
+triple_list=list(map(lambda x:x*3 , sample_list))
+print(triple_list)
+
+
+#solution for question3
+simple_list=[4, 5, 2, 9]
+square_list=list(map(lambda x:x**2 , simple_list))
+print(square_list)
